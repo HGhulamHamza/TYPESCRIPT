61e88bb6608fb9a52fa804d2fa9860a1b2c415a8
Hafiz Hamza <khawajahamza566@gmail.com>
2025-12-01 00:02:47 +0500

    done till 34:15

---
+++ one.js
@@ -0,0 +1,7 @@
+"use strict";
+//this is my first file,first code of Typescript
+console.log("Hello to Typescript");
+
+
+
+
--- one.ts
+++ one.ts
@@ -1,2 +1,15 @@
 //this is my first file,first code of Typescript
 console.log("Hello to Typescript")
+
+
+
+
+//primitive and reference 
+// Primitive  89  
+// false
+// "egg"
+// He
+// Hamza
+
+// Reference  () { } []  
+
