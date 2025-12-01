4342b1d32e7b97148734e57dba17b30a93c118fe
Hafiz Hamza <khawajahamza566@gmail.com>
2025-12-01 23:46:28 +0500

    Done till 55:45

--- one.js
+++ one.js
@@ -1,7 +1,26 @@
-"use strict";
 //this is my first file,first code of Typescript
 console.log("Hello to Typescript");
-
-
-
-
+//primitive and reference 
+// Primitive  89  
+// false
+// "egg"
+// He
+// Hamza
+// Reference  () { } []  
+// let arr =[1,2,3,"hamza"]    //typescript infers so type is string | number
+//Tuples
+var arr = ["hamza", 21]; //fixed and pehle se bataya ha k pehla string hu and dosra number hu
+//Enumes  is like key value pair laikn ham .laga k kc b value ko target kar sakte hain
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+// UserRoles.GUEST
+var StatusCodes;
+(function (StatusCodes) {
+    StatusCodes["ABANDONED"] = "abandoned status code 500";
+    StatusCodes["NOTFOUND"] = "not found status code 404";
+})(StatusCodes || (StatusCodes = {}));
+// StatusCodes.ABANDONED
--- one.ts
+++ one.ts
@@ -13,3 +13,46 @@ console.log("Hello to Typescript")
 
 // Reference  () { } []  
 
+
+
+// let arr =[1,2,3,"hamza"]    //typescript infers so type is string | number
+
+//Tuples
+let arr :[string,number]=["hamza",21]   //fixed and pehle se bataya ha k pehla string hu and dosra number hu
+
+
+//Enumes  is like key value pair laikn ham .laga k kc b value ko target kar sakte hain
+enum UserRoles{
+    ADMIN="admin",
+    GUEST="guest",
+    SUPER_ADMIN="super_admin"
+}
+// UserRoles.GUEST
+
+enum StatusCodes{
+    ABANDONED="abandoned status code 500",
+    NOTFOUND="not found status code 404"
+}
+// StatusCodes.ABANDONED
+
+let a:number;
+
+let b;   // ham ne isme bataya nahi ha k b ki type kia ha jaisay oper hamne a ki type bagaye k wo number ha to iski type ANY hogi
+
+a=12;
+
+let h:unknown;       //unknown type ha 
+h=12;
+h="hamza";
+if (typeof h=="string")
+    h.toUpperCase();
+
+
+
+//VOID
+
+function abcd(): void{
+       console.log("Ye coid fucntion ha kuch return nahi kar raha ha !")
+}
+
+
