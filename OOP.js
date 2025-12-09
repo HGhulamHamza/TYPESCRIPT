e1843b51bf456bcfda6aca5d092b51aba0152ae9
Hafiz Hamza <khawajahamza566@gmail.com>
2025-12-09 15:21:50 +0500

    Done till 2:02(till Access Modifiers)

--- OOP.js
+++ OOP.js
@@ -8,3 +8,47 @@ class Chips {
     }
 }
 let c1 = Chips;
+//Constructors
+class BottleMaker {
+    constructor(brand, price, material) {
+        this.brand = brand;
+        this.price = price;
+        this.material = material;
+    }
+}
+let bottle = new BottleMaker("Aqua", 200, "plastic");
+// class Music{
+//     public name,
+//     public artist,
+//     public thumbnail,
+//     public length,
+//     public available,
+//     constructor( name:string,  artist: string, thumbnail:string="somethumbnail.jpg", length:number ,public available:boolean){
+//             this.name=name;
+//             this.artist=artist;
+//             this.thumbnail=thumbnail;
+//             this.length=length;
+//             this.available=available;
+//     }
+// }
+//ANOTHER WAY OF DOING THE SAME THING
+//THIS REDUCES THE LINE OF CODE
+class Music {
+    constructor(name, artist, thumbnail = "somethumbnail.jpg", length, available) {
+        this.name = name;
+        this.artist = artist;
+        this.thumbnail = thumbnail;
+        this.length = length;
+        this.available = available;
+        if (!thumbnail) {
+            this.thumbnail = "somethumbnail.jpg"; //console ma ye show hoga q k thumbnail ka parameter empty ha 
+        }
+        if (!artist) {
+            this.artist = "Dont know the name";
+        }
+    }
+}
+let m1 = new Music("kali kali zulfo", "NFAK", "", 1200, false);
+let m3 = new Music("tu ha kaha", "", "", 1300, false);
+let m2 = new Music("kali kali zulfo", "NFAK", "", 1200, false);
+//ACCESS MODIFIERS
--- OOP.ts
+++ OOP.ts
@@ -7,3 +7,53 @@ class Chips {
 }
 
 let c1=Chips
+
+//Constructors
+class BottleMaker{
+    constructor(public brand: string,public price:number ,public material:string){
+
+    }
+}
+let bottle=new BottleMaker("Aqua",200,"plastic");
+
+// class Music{
+//     public name,
+//     public artist,
+//     public thumbnail,
+//     public length,
+//     public available,
+//     constructor( name:string,  artist: string, thumbnail:string="somethumbnail.jpg", length:number ,public available:boolean){
+//             this.name=name;
+//             this.artist=artist;
+//             this.thumbnail=thumbnail;
+//             this.length=length;
+//             this.available=available;
+
+//     }
+// }
+
+//ANOTHER WAY OF DOING THE SAME THING
+//THIS REDUCES THE LINE OF CODE
+class Music{
+    constructor(public name:string, public artist: string,public thumbnail:string="somethumbnail.jpg",public length:number ,public available:boolean){
+       if(!thumbnail){
+        this.thumbnail="somethumbnail.jpg";  //console ma ye show hoga q k thumbnail ka parameter empty ha 
+       }
+       if(!artist){
+        this.artist="Dont know the name";
+       }
+    }
+
+}
+let m1=new Music("kali kali zulfo","NFAK","",1200,false)
+let m3=new Music("tu ha kaha","","",1300,false)
+let m2=new Music("kali kali zulfo","NFAK","",1200,false)
+
+
+
+//ACCESS MODIFIERS
+
+
+
+
+
